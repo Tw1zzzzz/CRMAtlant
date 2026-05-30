a817c5c0a88941910f5acaa49f5a5be9854b3bfa
Twizz <twizz@MacBook-Air-Twizz.local>
2026-05-31 00:04:28 +0300

    Build image before stopping containers

--- deploy/scripts/deploy.sh
+++ deploy/scripts/deploy.sh
@@ -16,7 +16,7 @@ if ! grep -q "^VITE_YANDEX_METRIKA_ID=" ".env.production"; then
   echo "VITE_YANDEX_METRIKA_ID is not set in deploy/.env.production. Docker build will use default 109531131."
 fi
 
-docker compose -f docker-compose.prod.yml --env-file .env.production down --remove-orphans
 docker compose -f docker-compose.prod.yml --env-file .env.production build --pull
+docker compose -f docker-compose.prod.yml --env-file .env.production down --remove-orphans
 docker compose -f docker-compose.prod.yml --env-file .env.production up -d
 docker compose -f docker-compose.prod.yml --env-file .env.production ps
